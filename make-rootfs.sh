e8a6323f79b09b3bff8126b3168b900d444808f5
Noshiro Nanai <epm.maya@gmail.com>
2024-12-09 11:12:32 +0900

    Bump up v3.21

--- make-rootfs.sh
+++ make-rootfs.sh
@@ -2,7 +2,7 @@
 set -ex
 
 sudo ./alpine-make-rootfs \
-	--branch 'v3.20' \
+	--branch 'v3.21' \
 	--packages 'curl openssl openssh-client iptables xz git apk-tools' \
 	--script-chroot \
 	rootfs.tar.gz -- content/alpine.sh
